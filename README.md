bba2a49634436ce2ecae12a5004ff4a718eca1a3
River Alaqidy <riveralaqidy@Rivers-MacBook-Pro.local>
2026-02-08 15:19:49 -0600

    minimal shell completed with common commands

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # GoShell
 Minimalistic shell for UNIX(-like) operating systems in the Go programming language
+
+## Considered improvements
+
+
+1. Modify the input indicator:
+    *  add the working directory
+    * add the machine’s hostname
+    * add the current user
+2. Browse your input history with the up/down keys
+3. Add tab completion
--- main.go
+++ main.go
@@ -2,14 +2,41 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 func execInput(input string) error {
 	// remove the newline character
-	input = input.TrimSuffix(inputm, "\n")
+	input = strings.TrimSuffix(input, "\n")
 
+	// split input to separate the command and the arguments
+	args := strings.Split(input, " ")
+
+	switch args[0] {
+	case "cd":
+		// 'cd' tp home dir with empty path not yet supported
+		if len(args) < 2 {
+			return errors.New("path required")
+		}
+		// change the directory and return the error
+		return os.Chdir(args[1])
+	case "exit":
+		os.Exit(0)
+	}
+
+	// prepare command to execute
+	cmd := exec.Command(args[0], args[1:]...)
+
+	// set the correct output device
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	// execute the command and return the error
+	return cmd.Run()
 }
 
 func main() {
@@ -18,11 +45,15 @@ func main() {
 
 	// shell prompt
 	for {
+		fmt.Print("> ")
 		// read current input
 		input, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
-		execInput(input)
+		// handle execution of the input
+		if err = execInput(input); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
 	}
 }
